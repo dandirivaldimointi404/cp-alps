e7f7afc080e80c8daa5d029e7d9ceac8a6900be9
dandirivaldimointi404 <hilenkelo0505@gmail.com>
2024-04-25 23:40:50 +0800

    hujaantiba

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -14,22 +14,45 @@
         <div class="header-content py-6 is-dark mt-lg-n1 bg-dark mt-n3">
             <div class="container">
                 <div class="row flex-row-reverse justify-content-center g-gs">
+
                     <div class="col-lg-12 col-md-7">
                         <div class="header-caption">
                             @php
                                 $postBlog = $blog->last();
                             @endphp
-                            <div class="img-block mb-4 rounded-4" style="max-width: 100%; overflow: hidden; height: 400px;">
+                            <div class="img-block mb-4 rounded-4"
+                                style="max-width: 100%; overflow: hidden; height: 400px; position: relative;">
                                 <img src="{{ asset('uploads/' . $postBlog->gambar) }}" class="img-fluid" alt="Your Image"
                                     style="width: 100%; height: 100%; object-fit: cover; object-position: center;">
+                                <div class="text-overlay"
+                                    style="position: absolute; bottom: 0; left: 0; width: 100%; height: 40%; background-color: rgba(0, 0, 0, 0.5); color: #fff; display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center;">
+                                    <small>{{ $postBlog->kategori }}</small>
+                                    <h3 class="title" style="margin-top: 10px;">
+                                        <a href="{{ route('blog.show', $postBlog->slug) }}"
+                                            style="text-decoration: none; color: rgb(255, 255, 255);">
+                                            {{ $postBlog->judul }}
+                                        </a>
+                                    </h3>
+                                </div>
                             </div>
-                            
-                            <small>{{ $postBlog->kategori }}</small>
-                            <h3 class="title"><a href="{{ route('blog.show', $postBlog->slug) }}"
-                                    style="text-decoration: none; color: rgb(255, 255, 255);">{{ $postBlog->judul }}</a>
-                            </h3>
                         </div><!-- .header-caption -->
                     </div><!-- .col -->
+
+                    {{-- <div class="img-block mb-4 rounded-4"
+                        style="max-width: 100%; overflow: hidden; height: 400px; position: relative;">
+                        <img src="{{ asset('uploads/' . $postBlog->gambar) }}" class="img-fluid" alt="Your Image"
+                            style="width: 100%; height: 100%; object-fit: cover; object-position: center;">
+                        <div class="text-overlay"
+                            style="position: absolute; bottom: 0; left: 0; width: 100%; height: 100%; background-image: linear-gradient(to top, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0) 100%); color: #fff; display: flex; flex-direction: column; justify-content: flex-end; align-items: center; text-align: center;">
+                            <small>{{ $postBlog->kategori }}</small>
+                            <h3 class="title" style="margin-top: 10px;">
+                                <a href="{{ route('blog.show', $postBlog->slug) }}"
+                                    style="text-decoration: none; color: rgb(255, 255, 255);">
+                                    {{ $postBlog->judul }}
+                                </a>
+                            </h3>
+                        </div>
+                    </div> --}}
 
                     <div id="carouselExampleSlidesOnly" class="carousel slide col-lg-12" data-bs-ride="carousel">
                         <div class="carousel-inner">
@@ -41,13 +64,15 @@
                                             <div class="col-3">
                                                 <div class="review">
                                                     <div class="review-brand">
-                                                        <img src="{{ asset('uploads/' . $item->gambar) }}" alt="img-artikel" class="rounded-4">
+                                                        <img src="{{ asset('uploads/' . $item->gambar) }}" alt="img-artikel"
+                                                            class="rounded-4"
+                                                            style="width: 100%; height: 150px; object-fit: cover;">
                                                     </div>
                                                     <small>{{ $item->kategori }}</small>
                                                     <br>
                                                     <h6 class="fw-bold"><a href="{{ route('blog.show', $item->slug) }}"
-                                                        style="text-decoration: none; color: rgb(255, 255, 255);">{{ Str::limit($item->judul, 50) }}</a>
-                                                </h6>
+                                                            style="text-decoration: none; color: rgb(255, 255, 255);">{{ Str::limit($item->judul, 50) }}</a>
+                                                    </h6>
                                                 </div>
                                             </div>
                                         @endforeach
@@ -97,6 +122,8 @@
                 </div><!-- .col -->
             </div><!-- .row -->
         </div><!-- .container -->
+
+
     </header>
 
 
@@ -154,6 +181,7 @@
         </div><!-- .container -->
     </section><!-- .section --> --}}
 
+
     <section class="section section-cta">
         <div class="container">
             <h3 class="title text-dark">Artikel Terbaru</h3>
@@ -161,30 +189,33 @@
             <div class="row">
                 @foreach ($blog->chunk(2) as $chunk)
                     @foreach ($chunk as $item)
-                        <div class="col-md-6">
-                            <div class="row">
-                                <div class="col-xl-4 col-lg-5 col-md-4">
-                                    <div class="img-block h-80px mb-4 rounded">
-                                        <img src="{{ asset('uploads/' . $item->gambar) }}" class="img-fluid"
-                                            alt="Your Image">
-                                    </div>
-                                </div><!-- .col -->
-                                <div class="col-xl-8 col-lg-7 col-md-8">
-                                    <div class="text-block is-compact">
-                                        <small class="d-block date text">
-                                            <small
-                                                class="text-uppercase border-end brd-gray pe-3 me-3 color-blue4 fw-bold">{{ $item->kategori }}</small>
-                                            <small>{{ $item->created_at->subHour()->locale('id')->diffForHumans() }}</small>
-                                        </small>
-                                        <br>
-                                        <h5 class="title"><a href="{{ route('blog.show', $item->slug) }}"
-                                                style="text-decoration: none; color: black;">{{ $item->judul }}</a></h5>
-                                        {{-- {!! Str::limit($item->konten, 250) !!} --}}
-                                    </div>
-                                </div><!-- .col -->
-                            </div><!-- .row -->
+                        {{-- <div class="row flex-row-reverse align-items-center justify-content-between g-gs"> --}}
+                        <div class="col-lg-6">
+                            <div class="text-block">
+                                <div class="g-gs">
+                                    <div class="service service-s3">
+                                        <div class="col-xl-4 col-lg-5 col-md-4">
+                                            <div class="img-block h-80px mb-4 rounded">
+                                                <img src="{{ asset('uploads/' . $item->gambar) }}" class="img-fluid"
+                                                    alt="Your Image"
+                                                    style="width: 200px; height: 150px; object-fit: cover;">
+                                            </div>
+                                        </div>
+                                        <div class="service-text p-2 col-xl-8 col-lg-5 col-md-4">
+                                            <small class="d-block date text">
+                                                <small class="text-uppercase border-end brd-gray pe-3 me-3 color-blue4 fw-bold">{{ $item->kategori }}</small>
+                                                <small>{{ $item->created_at->subHour()->locale('id')->diffForHumans() }}</small>
+                                            </small>
+                                            <small class="fw-bold"><a href="{{ route('blog.show', $item->slug) }}"
+                                                    style="text-decoration: none; color: black;">{{ $item->judul }}</a>
+                                            </small>
+                                        </div>
+                                    </div><!-- .service -->
+                                </div>
+                            </div><!-- .text-block -->
                             <hr>
-                        </div><!-- .col-md-6 -->
+                        </div><!-- .col -->
+                        {{-- </div><!-- .row --> --}}
                     @endforeach
                 @endforeach
             </div><!-- .row -->
@@ -195,7 +226,7 @@
     <section class="section section-cta">
         <div class="container">
             <div class="row">
-                
+
                 <div class="col-xl-6 col-lg-7 col-md-8">
                     <div class="text-block is-compact">
                         <h2 class="title">Punya Pertanyaan? Team Kami Siap Membantu Kamu.</h2>
